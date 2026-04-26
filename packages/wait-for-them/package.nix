81b3af4a7862adec01fcf3a0c6f62c4656e153f3
Ifiok Jr. <ifiokotung@gmail.com>
2026-04-27 00:59:29 +0100

    fix(wait-for-them): build from source to support all platforms

--- packages/wait-for-them/package.nix
+++ packages/wait-for-them/package.nix
@@ -1,63 +1,33 @@
 {
-  stdenv,
-  fetchurl,
-  autoPatchelfHook,
-  openssl,
   lib,
+  rustPlatform,
+  fetchFromGitHub,
+  pkg-config,
+  openssl,
 }:
 
-let
+rustPlatform.buildRustPackage (finalAttrs: {
+  pname = "wait-for-them";
   version = "0.5.1";
 
-  platformSuffix =
-    {
-      "x86_64-darwin" = "macos";
-      "x86_64-linux" = "linux";
-    }
-    .${stdenv.hostPlatform.system} or (throw "Unsupported platform: ${stdenv.hostPlatform.system}");
-
-  hashes = {
-    "macos" = "sha256-cBCRMGX4JM75xHNmjTDKKvDuzKlS7+s1a2YF0kFM7ew=";
-    "linux" = "sha256-wiGuTSWXvZGUPJ/v1gMGAz1v4eX4OABnko7CXPsAv6A=";
-  };
-in
-stdenv.mkDerivation {
-  pname = "wait-for-them";
-  inherit version;
-
-  src = fetchurl {
-    url = "https://github.com/shenek/wait-for-them/releases/download/v${version}/wait-for-them-${platformSuffix}";
-    sha256 = hashes.${platformSuffix} or lib.fakeSha256;
+  src = fetchFromGitHub {
+    owner = "shenek";
+    repo = "wait-for-them";
+    rev = "v${finalAttrs.version}";
+    hash = "sha256-Ckw97tR04Sr5eoRX11dswk3+4RvQrA4rI9gZR5xd54E=";
   };
 
-  dontUnpack = true;
-  dontBuild = true;
-  dontStrip = stdenv.isDarwin;
+  cargoHash = "sha256-0ToxCKvjhfz8c6RuWN+DT+9bmfIn9wQomzI9lslDtn4=";
 
-  nativeBuildInputs = lib.optionals stdenv.isLinux [ autoPatchelfHook ];
-  buildInputs = lib.optionals stdenv.isLinux [
-    stdenv.cc.cc.lib
-    openssl
-  ];
+  nativeBuildInputs = [ pkg-config ];
+  buildInputs = [ openssl ];
 
-  installPhase = ''
-    runHook preInstall
+  doCheck = false;
 
-    mkdir -p $out/bin
-    cp $src $out/bin/wait-for-them
-    chmod +x $out/bin/wait-for-them
-
-    runHook postInstall
-  '';
-
-  meta = with lib; {
+  meta = {
     description = "Wait until all provided host:port pairs are opened or HTTP URLs return 200";
     homepage = "https://github.com/shenek/wait-for-them";
-    license = licenses.gpl3Only;
-    platforms = [
-      "x86_64-linux"
-      "x86_64-darwin"
-    ];
+    license = lib.licenses.gpl3Only;
     maintainers = [ ];
     mainProgram = "wait-for-them";
     tags = [
@@ -66,4 +36,4 @@ stdenv.mkDerivation {
       "networking"
     ];
   };
-}
+})
